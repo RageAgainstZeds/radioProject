f82f06c70fc0806df730ca4198f8c6f1d29f1184
RageAgainst <uuvulaulu@gmail.com>
2018-02-18 23:27:16 +0200

    first commit

---
+++ AudioPlayer/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String aud = "audio/love.wav";
+		AudioPlayerClass.playSound(aud).join();
+		
+		
+	}
+
+}
